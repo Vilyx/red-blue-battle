0ebec4d18332395ef863bdb5fbb6528854e4a620
Mikhail Nikitin <vilyx@yandex.ru>
2024-03-27 23:59:12 +0300

    Доработка для teamcity

---
+++ Assets/Editor/BuildToolExample.cs
@@ -0,0 +1,43 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+using System.IO;
+
+public class BuildToolExample : MonoBehaviour
+{
+    [MenuItem("Build/Build Windows")]
+    public static void MyBuild()
+    {
+        // ===== This sample is taken from the Unity scripting API here:
+        // https://docs.unity3d.com/ScriptReference/BuildPipeline.BuildPlayer.html
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/Main.unity"};
+        var buildPath = Application.dataPath + "/../Builds/mygame.exe";
+        buildPlayerOptions.locationPathName = buildPath;
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        if (!File.Exists(buildPath))
+        {
+            File.Delete(buildPath);
+        }
+
+        if (Directory.Exists(buildPath))
+        {
+            Directory.Delete(buildPath, true);
+        }
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
